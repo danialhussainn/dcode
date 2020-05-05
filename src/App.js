2248627c311d928c2f9608b9a5ff1cd5b44259f6
danialhussainn <danialhussainn@gmail.com>
2020-05-04 21:36:18 -0500

    this

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './component/login/login'
+class App extends React.Component
+{
+  render(){ console.log
+
+     return (
+     <div><Login/>this is </div>
+     )
+  }
+}
+export default App;
---
+++ src/component/login/login.js
@@ -0,0 +1,19 @@
+import React from 'React'
+
+class Login extends React.Component
+{
+    render()
+    {
+        return( 
+        <div><h1>this is one</h1></div>
+
+
+
+        ) 
+
+    }
+
+
+
+}
+export default Login;
